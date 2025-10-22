74cf526b2d58156dcae14f21e95d7871d0aa89c0
John Anigbogu, Jr. <johnoaj11@gmail.com>
2025-10-21 20:22:42 -0400

    John Anigbogu

---
+++ workers-view-prev-apptmts.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+
+app = Flask(__name__)
+CORS(app)
+
+db = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
+)
+
+cursor = db.cursor()
+#clients can browse salons
+@app.route("/api/workers/view-prev-appointments", methods=["GET"])
+def worker_view_appoints():
+    query = """ 
+select a.aid as appointment_id, s.name as service_name, s.price as service_price, u.first_name, 
+u.last_name, c.cid as customer_id, a.start_time, a.expected_end_time, a.end_time
+from appointments a
+join customers c on a.cid = c.cid
+join services s on a.sid = s.sid
+join users u on c.uid = u.uid
+"""
+    cursor.execute(query)
+    rows = cursor.fetchall()    
+    appointments = [{"appointment_id": row[0], "service_name": row[1], 
+                "service_price": row[2], "first_name": row[3],
+                "last_name": row[4], "customer_id": row[5],
+                "start_time": row[6], "expected_end_time": row[7],
+                "end_time": row[8]
+                } for row in rows]
+    return jsonify(appointments)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
