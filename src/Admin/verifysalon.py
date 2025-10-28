67f25e18bc57585bf6866612922d571ce0dbd5c3
Devan Desai <devdes338@gmail.com>
2025-10-28 00:38:28 -0400

    verify salon

--- src/Admin/verifysalon.py
+++ src/Admin/verifysalon.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from flask_login import login_required
+from flask_login import login_required, current_user
 from .queries import *
 from .admin_func import *
 
@@ -8,6 +8,11 @@ verifysalon = Blueprint("verifysalon",__name__,url_prefix='admin')
 @verifysalon.route('/approve-salon')
 @login_required
 def verify_salon():
+    if(current_user.role != "admin"):
+        return jsonify({
+            "status": "failure",
+            "message": "unauthorized access"
+        }), 401
     data = request.get_json()
     try:
         conn = get_db_connection()
@@ -19,8 +24,11 @@ def verify_salon():
             "User_ID": data['uid']
         }), 200
     except Error as e:
+        print(e)
         return jsonify({
-            "status":"failure"
+            "status":"failure",
+            "message":"Database Error",
+            "Error": e
         }), 400
     
     
