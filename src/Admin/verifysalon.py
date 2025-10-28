ef891349269aa85147c00ffc5187f0766359e77c
Devan Desai <devdes338@gmail.com>
2025-10-27 23:19:49 -0400

    verify-salon in progress

--- src/Admin/verifysalon.py
+++ src/Admin/verifysalon.py
@@ -1,10 +1,20 @@
 from flask import Blueprint, request, jsonify
+from flask_login import login_required
+from .admin_func import *
 
 verifysalon = Blueprint("verifysalon",__name__,url_prefix='admin')
 
 @verifysalon.route('/approve-salon')
+@login_required
 def verify_salon():
     data = request.get_json()
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("UPDATE business JOIN users ON business.uid=users.uid SET status=TRUE WHERE users.uid=%s",[data['uid']])
+    except Error as e:
+        return 
+    
     
     
     
