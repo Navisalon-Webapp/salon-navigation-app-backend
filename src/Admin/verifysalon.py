18ed282ff50050b16f3603ae8c594ca7e00ac389
Devan Desai <devdes338@gmail.com>
2025-10-27 22:06:28 -0400

    verifysalon in progress

--- src/Admin/verifysalon.py
+++ src/Admin/verifysalon.py
@@ -2,9 +2,10 @@ from flask import Blueprint, request, jsonify
 
 verifysalon = Blueprint("verifysalon",__name__,url_prefix='admin')
 
-@verifysalon.route('/approv-esalon')
+@verifysalon.route('/approve-salon')
 def verify_salon():
     data = request.get_json()
+    
     
     
     
