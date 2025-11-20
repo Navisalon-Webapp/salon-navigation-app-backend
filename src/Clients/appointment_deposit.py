6ed05c813afd9fe561a6e36fddd6985fc5aeac55
Devan Desai <devdes338@gmail.com>
2025-11-19 23:53:00 -0500

    insert new payment endpoint

--- src/Clients/appointment_deposit.py
+++ src/Clients/appointment_deposit.py
@@ -126,6 +126,59 @@ def payment_information(uid):
         if conn:
             conn.close()
 
-
+@deposit.route('/new-payment/<uid>', methods=['POST'])
+def insert_payment(uid):
+    data = request.get_json()
+    payment_type = data['payment_type'] if data['payment_type'] else None
+    card_number = data['card_number'] if data['card_number'] else None
+    if not payment_type or not card_number:
+        print("Missing parameters")
+        return jsonify({
+            "status":"success",
+            "message":"Missing parameters"
+        }), 400
+    
+    conn = None
+    cursor = None
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        insert_payment_info = """
+            insert into payment_information (uid, payment_type, card_number)
+            values (%s, %s, %s)
+        """
+        param = [uid, payment_type, card_number]
+        cursor.execute(insert_payment_info, param)
+        pay_id = cursor.lastrowid
+        conn.commit()
+        return jsonify({
+            "status":"success",
+            "message":"created new payment method for user",
+            "User ID": uid,
+            "Payment ID": pay_id
+        })
+    except mysql.connector.Error as e:
+        print(f"Database Error {e}")
+        if conn:
+            conn.rollback()
+        return jsonify({
+            "status":"failure",
+            "message":"Database Error",
+            "error": str(e)
+        }), 500
+    except Exception as e:
+        print(f"Error {e}")
+        if conn:
+            conn.rollback()
+        return jsonify({
+            "status":"failure",
+            "message":"Error",
+            "error": str(e)
+        }), 500
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
     
     
