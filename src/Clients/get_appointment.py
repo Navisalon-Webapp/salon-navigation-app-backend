ef6f15d97c638e139794d46ae9fa77768876d79c
unknown <tgammer0415@gmail.com>
2025-12-02 03:06:54 -0500

    Added port to connections

--- src/Clients/get_appointment.py
+++ src/Clients/get_appointment.py
@@ -14,6 +14,7 @@ def get_db():
         db = mysql.connector.connect(
             host=os.getenv("DB_HOST"),
             user=os.getenv("DB_USER"),
+            port=int(os.getenv("DB_PORT")),
             password=os.getenv("DB_PASSWORD"),
             database=os.getenv("DB_NAME")
         )
